03c0041c23207764db8f0a5ba296c11aa2382e25
Taylor Blocker <taylorblocker@gmail.com>
2019-11-01 13:07:18 -0500

    committing changes to exercise 1

--- arrowFunctions/ex1.js
+++ arrowFunctions/ex1.js
@@ -2,18 +2,18 @@
 // Spice them up by converting them to arrow functions!
 
 
-let introduction = function(name, otherName) {
+let introduction = (name, otherName) => {
     let introduction = name + ", let me introduce you to " + otherName;
-    return introduction;
+    return introduction; 
 }
 
 // This one only has one parameter! 
-let boringFunction = function (topic) {
+let boringFunction = topic => {
     let complain = topic + " is so boooooring!!!";
     return complain;
 }
 
 // This one has a single return statement!
-let shout = function() {
+let shout = () => {
     return "AAAHHHH!!"
 }
